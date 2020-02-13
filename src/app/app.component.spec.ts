ba634ab0129a580dacaf59cdef97003a74309e4c
Sai Nadkarni <sai2010nadkarni@gmail.com>
2020-02-13 23:29:14 +0530

    change name reference everywhere

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chatbot'`, () => {
+  it(`should have as title 'Arc'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('chatbot');
+    expect(app.title).toEqual('Arc');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to chatbot!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Arc!');
   });
 });
